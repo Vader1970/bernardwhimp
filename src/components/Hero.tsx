d2c7547f30ca6872a777dd136f6905b51f84f540
Vader1970 <daniel.wilkey@gmail.com>
2025-02-10 17:30:39 +1300

    Added Layout102 and Content1

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -21,7 +21,8 @@ type Props = {
   image: ImageProps;
 };
 
-export type HeroProps = React.ComponentPropsWithoutRef<"section"> & Partial<Props>;
+export type HeroProps = React.ComponentPropsWithoutRef<"section"> &
+  Partial<Props>;
 
 export const Hero = (props: HeroProps) => {
   const { tagline, heading, buttons, image } = {
@@ -35,27 +36,31 @@ export const Hero = (props: HeroProps) => {
   };
 
   return (
-    <section id='relume' className='relative px-[5%]'>
-      <div className='container'>
-        <div className='flex min-h-svh flex-col justify-end md:justify-end lg:justify-start lg:flex-row lg:items-center py-16 md:py-24 lg:py-28'>
-          <div className='max-w-md pb-8 md:pb-12 lg:pb-0'>
-            <p className='mb-3 font-normal text-text-alternative md:mb-4'>{tagline}</p>
-            <h1 className='mb-5 text-6xl font-bold text-text-alternative md:mb-6 md:text-9xl lg:text-10xl'>
+    <section id="relume" className="relative px-[5%]">
+      <div className="container">
+        <div className="flex min-h-svh flex-col justify-end md:justify-end lg:justify-start lg:flex-row lg:items-center py-16 md:py-24 lg:py-28">
+          <div className="max-w-md pb-8 md:pb-12 lg:pb-0">
+            <p className="mb-3 font-normal text-text-alternative md:mb-4">
+              {tagline}
+            </p>
+            <h1 className="mb-5 text-6xl font-bold text-text-alternative md:mb-6 md:text-9xl lg:text-10xl">
               {heading}
             </h1>
-            <p className='text-text-alternative md:text-md'>
-              Bernard Whimp has over 30 years experience in share investment and stock markets, commercial property
-              investment and development, commercial property financing and mortgage lending, property <br />
+            <p className="text-text-alternative md:text-md">
+              Bernard Whimp has over 30 years experience in share investment and
+              stock markets, commercial property investment and development,
+              commercial property financing and mortgage lending, property{" "}
+              <br />
               and securities law.
             </p>
-            <div className='mt-6 flex flex-wrap gap-4 md:mt-8'>
+            <div className="mt-6 flex flex-wrap gap-4 md:mt-8">
               {buttons.map((button, index) =>
                 button.title === "Learn more" ? (
                   <Button key={index} {...button} onClick={handleLearnMore}>
                     {button.title}
                   </Button>
                 ) : (
-                  <Button key={index} {...button} href='/contact'>
+                  <Button key={index} {...button} href="/contact">
                     {button.title}
                   </Button>
                 )
@@ -64,16 +69,16 @@ export const Hero = (props: HeroProps) => {
           </div>
         </div>
       </div>
-      <div className='absolute inset-0 -z-10'>
+      <div className="absolute inset-0 -z-10">
         <Image
           src={image.src}
           alt={image.alt || "Background image"}
           fill
-          sizes='(max-width: 768px) 100vw, (max-width: 1200px) 100vw, 100vw'
-          className='object-cover object-[65%_center] md:object-center'
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 100vw, 100vw"
+          className="object-cover object-[65%_center] md:object-center"
           priority
         />
-        <div className='absolute inset-0 bg-gradient-to-t from-black/80 via-black/40 to-transparent' />
+        <div className="absolute inset-0 bg-gradient-to-t from-black/80 via-black/40 to-transparent" />
       </div>
     </section>
   );
@@ -87,7 +92,7 @@ export const HeroDefaults: Props = {
     { title: "Get in touch", variant: "transparent-bg", href: "#contact" },
   ],
   image: {
-    src: "/images/hero-crop.jpg",
+    src: "/images/hero.webp",
     alt: "Bernard Whimp",
   },
 };
--- src/components/Layout102.tsx
+++ src/components/Layout102.tsx
@@ -17,7 +17,8 @@ type Props = {
   image: ImageProps;
 };
 
-export type Layout102Props = React.ComponentPropsWithoutRef<"section"> & Partial<Props>;
+export type Layout102Props = React.ComponentPropsWithoutRef<"section"> &
+  Partial<Props>;
 
 export function Layout102(props: Layout102Props) {
   const { heading, description, image, subHeadings } = {
@@ -26,49 +27,57 @@ export function Layout102(props: Layout102Props) {
   };
 
   return (
-    <section id='layout102' className='px-[5%] py-16 md:py-24 lg:py-28'>
-      <div className='container'>
-        <div className='mb-12 grid grid-cols-1 items-start justify-between gap-x-12 gap-y-5 md:mb-18 md:grid-cols-2 md:gap-x-12 md:gap-y-8 lg:mb-20 lg:gap-x-20'>
+    <section id="layout102" className="px-[5%] py-16 md:py-24 lg:py-28">
+      <div className="container">
+        <div className="mb-12 grid grid-cols-1 items-start justify-between gap-x-12 gap-y-5 md:mb-18 md:grid-cols-2 md:gap-x-12 md:gap-y-8 lg:mb-20 lg:gap-x-20">
           <header>
-            <h2 className='text-4xl font-bold leading-[1.2] md:text-5xl lg:text-6xl'>{heading}</h2>
+            <h2 className="text-4xl font-bold leading-[1.2] md:text-5xl lg:text-6xl">
+              {heading}
+            </h2>
           </header>
           <div>
-            <p className='mb-6 md:mb-8 md:text-md'>{description}</p>
-            <div className='grid grid-cols-1 gap-6 py-2 sm:grid-cols-2'>
+            <p className="mb-6 md:mb-8 md:text-md">{description}</p>
+            <div className="grid grid-cols-1 gap-6 py-2 sm:grid-cols-2">
               {subHeadings.map((subHeading, index) => (
                 <article key={index}>
-                  <h3 className='mb-3 text-md font-bold leading-[1.4] md:mb-4 md:text-xl'>{subHeading.title}</h3>
+                  <h3 className="mb-3 text-md font-bold leading-[1.4] md:mb-4 md:text-xl">
+                    {subHeading.title}
+                  </h3>
                   <p>{subHeading.description}</p>
                 </article>
               ))}
             </div>
           </div>
         </div>
-        <div className='relative'>
-          <div className='absolute inset-0 bg-black/10 z-10'></div>
+        <div className="relative">
+          <div className="absolute inset-0 bg-black/10 z-10"></div>
           <Image
             src={image.src}
             alt={image.alt ?? ""}
             width={1280}
-            height={720}
-            className='w-full object-cover relative rounded-lg'
+            height={853}
+            className="w-full object-cover relative rounded-lg"
           />
           {/* Glass morphism quote on medium devices and up */}
-          <div className='absolute left-[5%] top-1/2 -translate-y-1/2 lg:max-w-[300px] md:max-w-[270px] h-auto rounded-lg bg-white/10 backdrop-blur-md p-8 hidden md:flex flex-col justify-center'>
-            <p className='text-white text-2xl italic font-normal mb-4'>
-              &quot;I am looking to build on the knowledge and experience I have accumulated to create life changing
-              wealth for investors&quot;
+          <div className="absolute left-[5%] top-1/2 -translate-y-1/2 lg:max-w-[300px] md:max-w-[270px] h-auto rounded-lg bg-white/10 backdrop-blur-md p-8 hidden md:flex flex-col justify-center">
+            <p className="text-white text-2xl italic font-normal mb-4">
+              &quot;I am looking to build on the knowledge and experience I have
+              accumulated to create life changing wealth for investors&quot;
             </p>
-            <p className='text-white italic font-normal mb-4 text-lg'>- Bernard Whimp</p>
+            <p className="text-white italic font-normal mb-4 text-lg">
+              - Bernard Whimp
+            </p>
           </div>
         </div>
         {/* Quote for small devices */}
-        <div className='bg-white mt-6 md:hidden'>
-          <p className='text-black italic font-normal mb-4 sm:text-2xl'>
-            &quot;I am looking to build on the knowledge and experience I have accumulated to create life changing
-            wealth for investors&quot;
+        <div className="bg-white mt-6 md:hidden">
+          <p className="text-black italic font-normal mb-4 sm:text-2xl">
+            &quot;I am looking to build on the knowledge and experience I have
+            accumulated to create life changing wealth for investors&quot;
           </p>
-          <p className='text-black italic font-normal mb-4 sm:text-2xl'>- Bernard Whimp</p>
+          <p className="text-black italic font-normal mb-4 sm:text-2xl">
+            - Bernard Whimp
+          </p>
         </div>
       </div>
     </section>
@@ -76,10 +85,10 @@ export function Layout102(props: Layout102Props) {
 }
 
 export const Layout102Defaults: Props = {
-  heading: `Described by some as "the god of stocks."`,
-  description: `Bernard Whimp is a visionary and an entrepreneur, some would refer to him as "the god of stocks". As an experienced professional stock picker, he is passionate, relentless, and strategic when it comes to the Australian stock market.`,
+  heading: `Described by some as "the god of stocks"`,
+  description: `Bernard Whimp is a visionary and an entrepreneur; some would refer to him as "the god of stocks". As an experienced professional stock picker, he is passionate, relentless, and strategic when it comes to the Australian stock market.`,
   image: {
-    src: "/images/bernard-desk-center.webp",
+    src: "/images/bernard-at-desk.webp",
     alt: "Bernard sitting at desk",
   },
   subHeadings: [
